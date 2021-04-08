f1dab4833830f85a981b8ce57e5c4da7c0c8c786
Bl00mstar <mjakstefan02@gmail.com>
2021-04-08 20:59:21 +0200

    .net rest up

--- .net_api/Api/Program.cs
+++ .net_api/Api/Program.cs
@@ -21,6 +21,7 @@ namespace Commander
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://0.0.0.0:5001");
                 });
     }
 }
